98e8af981673817f054a4e210febc423f68ec5f6
Yauheni Valiantsinovich <zhenya.val.77@gmail.com>
2025-05-11 23:03:57 +0300

    added to api swagger and dbContext ( with connection string in json)

--- ExpenseTracker.WebApi/Program.cs
+++ ExpenseTracker.WebApi/Program.cs
@@ -1,6 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using ExpenseTracker.Persistence;
+using Microsoft.EntityFrameworkCore;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = builder.Configuration;
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString(nameof(ApplicationDbContext)));
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
 
 app.Run();
